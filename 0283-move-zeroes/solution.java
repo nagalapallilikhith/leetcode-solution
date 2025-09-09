7a7b2d99089a42974a7f175bbe8ecfabc5e5a068
nagalapallilikhith <nagalapallilikhith@gmail.com>
2025-09-09 21:04:18 +0000

    Sync LeetCode submission Runtime - 12 ms (7.53%), Memory - 45.6 MB (99.95%)

---
+++ 0283-move-zeroes/solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]!=0){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
