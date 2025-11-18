23674079b8071b1bdc9fb939237ea34cf74c90bd
nagalapallilikhith <nagalapallilikhith@gmail.com>
2025-11-18 17:06:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.6 MB (27.96%)

---
+++ 0717-1-bit-and-2-bit-characters/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int n=bits.length,i=0;
+        while(i<n-1){
+            if(bits[i]==1){
+                i+=2;
+            }else{
+                i+=1;
+            }
+        }return i==n-1;
+    }
+}
