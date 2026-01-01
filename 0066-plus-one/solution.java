98a2bf0cdac7300a37917270c438bc362abf79b0
nagalapallilikhith <nagalapallilikhith@gmail.com>
2026-01-01 17:25:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.4 MB (61.72%)

--- 0066-plus-one/solution.java
+++ 0066-plus-one/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        
-        for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;    
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
                 return digits;
             }
-            digits[i] = 0;
+            digits[i]=0;
         }
-        int[] res = new int[n + 1];
-        res[0] = 1;
+        int res[]=new int[digits.length+1];
+        res[0]=1;
         return res;
     }
 }
